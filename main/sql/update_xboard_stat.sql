b4d80395abc07f603831ce6af40552c225447503
bawi service acccount <bawi@orange.bawi.org>
2017-10-10 17:52:30 +0900

    Fix SQL bug due to MySQL 5.7 upgrade
    See https://stackoverflow.com/questions/34115174/error-related-to-only-full-group-by-when-executing-a-query-in-mysql
    
    (cherry picked from commit 90286be65a318d3d0dc083cfec6af6b28ad9d4bb)

--- main/sql/update_xboard_stat.sql
+++ main/sql/update_xboard_stat.sql
@@ -4,11 +4,11 @@ insert into bw_xboard_stat_board (board_id, counts, articles, comments, recoms)
   select a.board_id, sum(count) as counts, count(b.article_id) as articles, sum(comments) as comments, sum(recom) as recoms
     from bw_xboard_board as a, bw_xboard_header as b
       where a.board_id=b.board_id && a.board_id !=637 && a.board_id != 688 && b.created> date_sub(now(), interval 7 day)
-        group by a.board_id order by count desc;
+        group by a.board_id order by counts desc;
 
-        delete from bw_xboard_stat_user;
+delete from bw_xboard_stat_user;
 
-        insert into bw_xboard_stat_user (id, name, articles, counts, comments, recoms)
-          select id, name, count(article_id) as articles, sum(count) as counts, sum(comments) as comments, sum(recom) as recoms
-            from bw_xboard_header where board_id not in (10, 1550) && created >  date_sub(now(), interval 7 day)
-              group by id order by counts desc;
+insert into bw_xboard_stat_user (id, name, articles, counts, comments, recoms)
+  select id, name, count(article_id) as articles, sum(count) as counts, sum(comments) as comments, sum(recom) as recoms
+    from bw_xboard_header where board_id not in (10, 1550) && created >  date_sub(now(), interval 7 day)
+      group by id, name order by counts desc;
